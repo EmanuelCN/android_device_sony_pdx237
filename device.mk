1daaa9eb61d2d2b7ed10b5f0a2476f986828f71d
EmanuelCN <emanuelghub@gmail.com>
2024-11-28 20:31:27 +0000

    pdx237: Inherit sony extra

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@
 # Inherit from sony sm8550-common
 $(call inherit-product, device/sony/sm8550-common/common.mk)
 
+# Inherit from sony extra
+$(call inherit-product, device/sony/extra/extra.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2520
 TARGET_SCREEN_WIDTH := 1080
